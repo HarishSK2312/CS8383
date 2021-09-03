b6f82eeca0c54d56a56063b700136168440268c0
HarishSK2312 <90037523+HarishSK2312@users.noreply.github.com>
2021-09-03 16:01:01 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,14 @@
+1.	class Student1{  
+2.	 int id;//data member (also instance variable)  
+3.	 String name;//data member(also instance variable)  
+4.	  
+5.	 public static void main(String args[]){  
+6.	  Student1 s1=new Student1();//creating an object of Student  
+7.	  System.out.println(s1.id);  
+8.	  System.out.println(s1.name);  
+9.	 }  
+10.	}  
+output:
+0
+
+null
