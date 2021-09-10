7a3925b1dec5a14a292544552b4d188049df0b4b
HarishSK2312 <90037523+HarishSK2312@users.noreply.github.com>
2021-09-10 19:59:12 +0530

    Create program24.java

---
+++ program24.java
@@ -0,0 +1,19 @@
+public class S {
+     	 void m(){  
+	  System.out.println("method is invoked");  
+	  }  
+	  void n(){  
+	  this.m();  
+	  }  
+	  void p(){  
+	  n();  
+	  }  
+	      
+    public static void main(String args[]) {
+    S s1 = new S();  
+	  s1.p();  
+	  
+    }
+}
+output:
+method is invoked
