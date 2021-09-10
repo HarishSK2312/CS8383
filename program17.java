8b58e5a839bfb54a0b809509744ef80920597090
HarishSK2312 <90037523+HarishSK2312@users.noreply.github.com>
2021-09-10 18:41:28 +0530

    Create program17.java

---
+++ program17.java
@@ -0,0 +1,19 @@
+public class Counter2  {
+       	static int count=0;//will get memory only once and retain its value  
+	  
+	Counter2(){  
+	count++;  
+	System.out.println(count);  
+	}  
+ 	
+    public static void main(String args[]) {
+	Counter2 c1=new Counter2();  
+	Counter2 c2=new Counter2();  
+	Counter2 c3=new Counter2();  
+  
+    }
+}
+output:
+1
+2
+3
