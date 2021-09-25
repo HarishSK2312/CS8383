15004cf854790f4766fa64f11c301708f9d7285d
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-25 22:50:48 +0530

    Create program45.java

---
+++ program45.java
@@ -0,0 +1,15 @@
+public class A{
+    			 
+    public static void main(String args[]) {
+       	StringBuffer sb=new StringBuffer();  
+	System.out.println(sb.capacity());  
+	sb.append("Hello");  
+	System.out.println(sb.capacity());  
+	sb.append("java is my favourite language");  
+	System.out.println(sb.capacity());
+	}  
+	}  
+output:
+16
+16
+34
