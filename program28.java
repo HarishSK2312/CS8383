af5a262e29c600331760027253ea95c1e007c0b0
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-25 21:26:39 +0530

    Create program28.java

---
+++ program28.java
@@ -0,0 +1,18 @@
+4.	int a[]=new int[5];//declaration and instantiation  
+5.	a[0]=10;//initialization  
+6.	a[1]=20;  
+7.	a[2]=70;  
+8.	a[3]=40;  
+9.	a[4]=50;  
+10.	  
+11.	//printing array  
+12.	for(int i=0;i<a.length;i++)//length is the property of array  
+13.	System.out.println(a[i]);  
+14.	  
+15.	}}  
+output:
+10
+20
+70
+40
+50
