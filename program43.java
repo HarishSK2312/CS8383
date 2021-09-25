a5bfae0d772a6fcd0914c44c72cf3fe2538ec92c
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-25 22:43:14 +0530

    Create program43.java

---
+++ program43.java
@@ -0,0 +1,11 @@
+public class TestSubstring{
+    			 
+    public static void main(String args[]) {
+       	   String s="Sachin Tendulkar";  
+	   System.out.println(s.substring(6));  
+	   System.out.println(s.substring(0,6));  
+	 }  
+	}  
+output:
+ Tendulkar
+Sachin
