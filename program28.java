b1fec7e4f26e25c28832924001ee3ff4e8b18bd8
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-25 21:30:21 +0530

    Update program28.java

--- program28.java
+++ program28.java
@@ -1,15 +1,16 @@
-4.	int a[]=new int[5];//declaration and instantiation  
-5.	a[0]=10;//initialization  
-6.	a[1]=20;  
-7.	a[2]=70;  
-8.	a[3]=40;  
-9.	a[4]=50;  
-10.	  
-11.	//printing array  
-12.	for(int i=0;i<a.length;i++)//length is the property of array  
-13.	System.out.println(a[i]);  
-14.	  
-15.	}}  
+	public class Testarray{
+  			
+    public static void main(String args[]) {int a[]=new int[5];  
+	a[0]=10;  
+	a[1]=20;  
+	a[2]=70;  
+	a[3]=40;  
+	a[4]=50;  
+	  	 
+	for(int i=0;i<a.length;i++)  
+	System.out.println(a[i]);  
+	  
+	}}  
 output:
 10
 20
