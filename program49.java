8e6769ec05ce907dca03904c155ea5d098707ce9
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-25 22:57:14 +0530

    Create program49.java

---
+++ program49.java
@@ -0,0 +1,10 @@
+public class A{
+    			 
+    public static void main(String args[]) {
+       			StringBuilder sb=new StringBuilder("Hello ");  
+	sb.append("Java");  
+	System.out.println(sb);  
+	}  
+	} 
+output:
+Hello Java
