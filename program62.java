03ad2944c1044475cdd212e32cf256cf94cf431b
Harish S K <90037523+HarishSK2312@users.noreply.github.com>
2021-09-27 19:02:33 +0530

    Create program62.java

---
+++ program62.java
@@ -0,0 +1,16 @@
+public class TestFinallyBlock {
+    	  
+    public static void main(String args[]) {
+    	  try{  
+	   int data=25/5;  
+	   System.out.println(data);  
+	  }  
+	  catch(NullPointerException e){System.out.println(e);}  
+	  finally{System.out.println("finally block is always executed");}  
+	  System.out.println("rest of the code...");  
+	  }  
+	} 
+output:
+5
+finally block is always executed
+rest of the code...
